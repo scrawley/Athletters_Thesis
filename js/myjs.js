c277f2dfe50828b0852803eb1251be441f0cda74
scrawley <scrawley@mica.edu>
2016-05-07 21:19:28 -0400

    Taking out repeat code in JS

--- js/myjs.js
+++ js/myjs.js
@@ -39,25 +39,7 @@
             return re.test(email);
         }
 		
-	$(document).ready(function() {
- 
-  $("#carousel_items").owlCarousel({
- 
-      navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true,
-	  autoPlay: 5000,
-	  paginationNumbers: true,
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
- 
-  });
+	
  
 });
 
