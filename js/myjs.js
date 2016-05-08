a72bad107cf5bb37d171cf25a4c6af4336d3135b
scrawley <scrawley@mica.edu>
2016-05-07 21:17:52 -0400

    Adding JS
    Added the js from book bridges.

--- js/myjs.js
+++ js/myjs.js
@@ -1,14 +1,63 @@
 // JavaScript Document
 
- $(document).ready(function() {
-      ;
-          
-     
-     
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+ 
+ 	
+	
+	
+	 $(window).load(function() {
+            toastr.options = {
+                "closeButton": false,
+                "debug": false,
+                "positionClass": "toast-top-right",
+                "onclick": null,
+                "showDuration": "300",
+                "hideDuration": "1000",
+                "timeOut": "5000",
+                "extendedTimeOut": "1000",
+                "showEasing": "swing",
+                "hideEasing": "linear",
+                "showMethod": "fadeIn",
+                "hideMethod": "fadeOut",
+            }
+            $('.blueberry').blueberry({
+                pager: false,
+                keynav: false
+            });
 
+            $('#newsletter-form').submit(function() {
+                var email = $('#newsletter-email').val();
+                if(!validateEmail(email)) {
+                    toastr.error("Please enter a valid email.");
+                    return false;
+                } 
+            });
+        });
+
+        function validateEmail(email) {
+            var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            return re.test(email);
+        }
+		
+	$(document).ready(function() {
+ 
+  $("#carousel_items").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true,
+	  autoPlay: 5000,
+	  paginationNumbers: true,
+ 
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
 });
-     
 
-
-    
-  
